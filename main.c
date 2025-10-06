4a62b59bf91729338d0b4c62e0187ce929fc3e9d
Heitor Henrique Klein <74987858+heitork1@users.noreply.github.com>
2025-10-06 10:19:53 -0300

    Update main.c

--- main.c
+++ main.c
@@ -3,30 +3,70 @@
 #include <string.h>
 #include <math.h>
 
+
 float calcDist(float  x1 ,float x2, float  x3 ,float x4, float  y1 ,float y2, float  y3 ,float y4 ){
     float dist = sqrt(pow(x1-y1, 2) + pow(x2-y2, 2) + pow(x3-y3, 2) + pow(x3-y3, 2));
     return (dist);
 }
 
+float calcAllDist(float matriz[150][4]){
+    int i;
+    int j;
+    float dist;
+    for(i = 0; i < 150; i++){
+        for(j = 0; j < 4){
+            dist = 
+        }
+    }
+}
+
+//para cada linha k, vou passar para calDist os 4 atributos de K + 
 //função que calcula o DEN e chama a função que cacula a distancia
+
 
 int main(){
     FILE * file; 
     char linha[256];
-
-    file = fopen("C:/Users/glize/Documents/TEG/IrisDataset.csv", "r");
+    int escolha;
+    float L;
+    float matriz[150][3];
+    
+    file = fopen("my_dataset.txt", "r");
     if(file == NULL){
         printf("Erro ao abrir o arquivo. \n");
         return 1;
     }
-    while (fgets(linha, sizeof(linha), file)){
-        // printf("%s", linha);
+    printf("Qual o L?\nDigite 1 para 0.0\nDigite 2 para 0.3\nDigite 3 para 0.5\n");
+    scanf("%d", &escolha);
+    
+    
+    
+    switch(escolha){
+        case 1:
+            L = 0.0;
+            break;
+        case 2:
+            L = 0.3;
+            break;
+        case 3:
+            L = 0.5;
+            break;
+        case 4:
+            L= 0.9;
+            break;
+        default:
+            printf("Escolha não definida");
     }
-
+    printf("%d\n", escolha);
+    printf("%.1f", L);
+    
+    while (fgets(linha, sizeof(linha), file)){
+        //printf("%s", linha);
+    }
+    
     fclose(file);
     float teste = calcDist(5.1,3.5,1.4,.2,4.9,3,1.4,.2);
-    printf("%f\n", teste);
+    //printf("%f\n", teste);
 
     return 0;
 }
-
